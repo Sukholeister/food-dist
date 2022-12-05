0802b19cd3f98ab7c1f8e35ac7a015a0e92322ea
Andrii <User80794@gmail.com>
2022-12-05 14:27:19 +0200

    render function const

--- js/script.js
+++ js/script.js
@@ -143,8 +143,11 @@ window.addEventListener('scroll', scrollModalWindow );
         }
 
     }
-
+    
     setClock(".timer", deadLine );
+
+
+
 
 
     function closeModal (){
@@ -166,6 +169,46 @@ window.addEventListener('scroll', scrollModalWindow );
                 }
        
     }
+
+    // class 
+
+    class MenuCard{
+        constructor(src,alt, title, descr, price, parentSelector){
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 40;
+        }
+        changeToUAH() {
+            this.price = this.price * this.transfer; 
+        }
+
+        render(){
+            const element = document.createElement('div');
+
+            element.innerHTML = `
+            <div class="menu__item">
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr"> ${this.descr} </div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+                </div>
+            `;
+                this.parent.append(element);
+        
+        }
+
+    }
+
+
+
 
 });
 
